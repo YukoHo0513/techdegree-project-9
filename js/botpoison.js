40dfe352274c1bb6a7fba465e4a39ea49c753a00
Ykhoo <89803794+YukoHo0513@users.noreply.github.com>
2022-03-17 11:17:39 -0700

    profile photo added to about section

---
+++ js/botpoison.js
@@ -0,0 +1,13 @@
+const formElement = document.getElementById("form");
+const buttonElement = document.getElementById("button");
+
+formElement.addEventListener("botpoison-challenge-start", function () {
+buttonElement.setAttribute("disabled", "disabled");
+});
+formElement.addEventListener("botpoison-challenge-success", function () {
+buttonElement.removeAttribute("disabled");
+});
+formElement.addEventListener("botpoison-challenge-error", function () {
+buttonElement.removeAttribute("disabled");
+});
+
